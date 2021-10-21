7836454651d2742b336b83133eb73385eb7f00df
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2021-10-20 22:01:30 -0400

    Created config folder with database.js file, moved sequelize and creating db there

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const exphbs = require('express-handlebars'); // express template engine to disp
 const bodyParser = require('body-parser'); // parses the incoming request bodies in a middleware (function that receives request and response objects) before you handle it
 const path = require('path'); // deals with filepaths
 const app = express(); // initialize app
-const { Sequelize } = require('sequelize');
 
 // database
+const db = require('./config/database');
 
-// FORMAT 1: const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
-const db = new Sequelize('postgres://lauren_eng:xTg9BV@localhost:5432/database_1882') 
-
-// FORMAT 2:
-// const db = new Sequelize('database_1882', 'lauren_eng', 'xTg9BV', {
-//         host: 'localhost',
-//         dialect: 'postgres'
-// }); 
 
 // test db 
 db.authenticate()
@@ -27,3 +19,8 @@ app.get('/', (req, res) => res.send('INDEX')); // create index route that catche
 const PORT = process.env.Port || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`)); // runs server
+
+
+// NEW MODULE
+
+// const db = require('.config/database');
